7ce6ba532238251eebec88bedb83406e5c095dbc
pedro <pfaria.sjp@ecori.com.br>
2025-03-17 09:15:52 -0300

    Criando testes unitarios

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterExpenseJsonBuilder.cs
@@ -0,0 +1,34 @@
+﻿
+
+using Bogus;
+using CashFlow.Communication.enums;
+using CashFlow.Communication.requests;
+
+namespace CommonTestUtilities.Requests
+{
+    public class RequestRegisterExpenseJsonBuilder
+    {
+
+        public static RequestRegisterExpenseJson Build() 
+        {
+            var faker = new Faker();
+
+            return new Faker<RequestRegisterExpenseJson>()
+                .RuleFor(r => r.Title, faker => faker.Commerce.ProductName())
+                .RuleFor(r => r.Description, faker => faker.Commerce.ProductDescription())
+                .RuleFor(r => r.Date, faker => faker.Date.Past())
+                .RuleFor(r => r.PaymentType, faker => faker.PickRandom<PaymentType>())
+                .RuleFor(r => r.Amount, faker => decimal.Parse(faker.Commerce.Price()));
+
+            //return new RequestRegisterExpenseJson 
+            //{
+            //    Amount = decimal.Parse(faker.Commerce.Price()),
+            //    Date = faker.Date.Past(),
+            //    Description = "",
+            //    PaymentType = CashFlow.Communication.enums.PaymentType.CreditCard,
+            //    Title = faker.Commerce.Product()
+            //};
+        }
+
+    }
+}
---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpensesValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using CashFlow.Application.useCases.Expenses.Register;
+using CashFlow.Communication.requests;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace Validators.Tests.Expenses.Register
+{
+    public class RegisterExpensesValidatorTests
+    {
+        [Fact]
+        public void Success()
+        {
+
+            // Arrange
+            var validator = new RegisterExpensesValidator();
+            var request = RequestRegisterExpenseJsonBuilder.Build();
+
+
+            // Act
+            var result = validator.Validate(request);
+
+            // Assert
+            //Assert.NotNull(result);
+            //Assert.True(result.IsValid);
+            result.IsValid.Should().BeTrue();
+        }
+    }
+}
