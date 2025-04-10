c31dba6c9f86e502d97071ca42c024c0da17fa6a
Pedro <p20142004@gmail.com>
2025-04-10 08:01:51 -0300

    Enhance password validation and update resource messages
    Updated PasswordValidator.cs to improve password validation logic, now checking for uppercase, lowercase, numbers, and special characters. Updated error message accordingly.
    
    Removed EMAIL_EMPTY, EMAIL_INVALID, INVALID_PASSWORD, and NAME_EMPTY localized string resources from ResourceReportGenerationMessages.Designer.cs and corresponding .resx files.
    
    Updated ResourceErrorMessages.Designer.cs and corresponding .resx files to reflect the new INVALID_PASSWORD message, which now includes the requirement for a special character.

--- src/CashFlow.Application/useCases/User/PasswordValidator.cs
+++ src/CashFlow.Application/useCases/User/PasswordValidator.cs
@@ -1,14 +1,60 @@
-﻿using FluentValidation;
+﻿using System.Text.RegularExpressions;
+using CashFlow.Exception;
+using FluentValidation;
 using FluentValidation.Validators;
 
 namespace CashFlow.Application.useCases.User;
 
 public class PasswordValidator<T> : PropertyValidator<T, string>
 {
+    private const string ERROR_MESSAGE_KEY = "ErrorMessage";
     public override string Name => "PasswordValidator";
 
-    public override bool IsValid(ValidationContext<T> context, string value)
+    protected override string GetDefaultMessageTemplate(string errorCode)
     {
-        return false;
+        return $"{{{ERROR_MESSAGE_KEY}}}";
+    }
+
+    public override bool IsValid(ValidationContext<T> context, string password)
+    {
+        if (string.IsNullOrEmpty(password))
+        {
+            context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, ResourceErrorMessages.INVALID_PASSWORD);
+            return false;
+        }
+
+        if (password.Length < 8)
+        {
+            context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, ResourceErrorMessages.INVALID_PASSWORD);
+            return false;
+        }
+
+        if (Regex.IsMatch(@"[A-Z]+", password) == false)
+        {
+            context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, ResourceErrorMessages.INVALID_PASSWORD);
+            return false;
+        }
+
+        if(Regex.IsMatch(@"[a-z]+", password) == false)
+        {
+            context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, ResourceErrorMessages.INVALID_PASSWORD);
+            return false;
+        }
+
+        if(Regex.IsMatch(@"[0-9]+", password) == false)
+        {
+            context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, ResourceErrorMessages.INVALID_PASSWORD);
+            return false;
+        }
+
+
+        if(Regex.IsMatch(@"[\!\?\.\*]+", password) == false)
+        {
+            context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, ResourceErrorMessages.INVALID_PASSWORD);
+            return false;
+        }
+
+
+        return true;
     }
 }
