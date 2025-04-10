d4b83eecdd729b39ecacabe866373f3cb9c36eb7
Pedro <p20142004@gmail.com>
2025-04-09 22:27:05 -0300

    Add database migration at startup
    Introduce database migration at application startup by adding a new `MigrateDatabase` method in `Program.cs`. This method creates a service scope and calls the `MigrateDatabase` method from the newly added `DataBaseMigration` class in `DataBaseMigration.cs`. The `DataBaseMigration` class retrieves the `CashFlowDbContext` and applies any pending migrations using `MigrateAsync`.

--- src/CashFlow.API/Program.cs
+++ src/CashFlow.API/Program.cs
@@ -2,6 +2,7 @@ using CashFlow.Api.Middleware;
 using CashFlow.API.Filters;
 using CashFlow.Application;
 using CashFlow.Infrastructure;
+using CashFlow.Infrastructure.Migrations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -35,4 +36,13 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+await MigrateDatabase();
+
 app.Run();
+
+
+async Task MigrateDatabase()
+{
+    await using var scope = app.Services.CreateAsyncScope();
+    await DataBaseMigration.MigrateDatabase(scope.ServiceProvider);
+}
