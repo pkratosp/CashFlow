83138a7291553f4e14f3c7d74dc426d318070cc9
Pedro <p20142004@gmail.com>
2025-04-23 23:18:51 -0300

    Add WebApi.Test project with initial test setup

---
+++ tests/WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Json;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace WebApi.Test.Users.Register;
+
+public class RegisterUserTest: IClassFixture<CustomWebApplicationFactory>
+{
+    private const string METHOD = "api/User";
+
+    private readonly HttpClient _httpClient;
+
+    public RegisterUserTest(CustomWebApplicationFactory webApplicationFactory)
+    {
+        _httpClient = webApplicationFactory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Sucess ()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+    }
+}
